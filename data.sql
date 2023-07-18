ed7f06ae3901afa253d7c22fd97645634d7217a3
wolterbwambale <benplaan1@gmail.com>
2023-07-19 00:00:20 +0300

    add data

--- data.sql
+++ data.sql
@@ -6,3 +6,13 @@ VALUES
   (2, 'Gabumon', '2018-11-15', 2, true, 8),
   (3, 'Pikachu', '2021-01-07', 1, false, 15.04),
   (4, 'Devimon', '2017-05-12', 5, true, 11);
+
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES(5, 'Charmander', '2020-02-08', 0, false, -11, 'Fire Lizard'),
+       (6, 'Plantmon', '2021-11-15', 2, true, -5.7, 'Plant Creature'),
+       (7, 'Squirtle', '1993-04-02', 3, false, -12.13, 'Water Turtle'),
+       (8, 'Angemon', '2005-06-12', 1, true, -45, 'Holy Angel'),
+       (9, 'Boarmon', '2005-06-07', 7, true, 20.4, 'Boar'),
+       (10, 'Blossom', '1998-10-13', 3, true, 17, 'Flower Fairy'),
+       (11, 'Ditto', '2022-05-14', 4, true, 22, 'Transforming Blob');
